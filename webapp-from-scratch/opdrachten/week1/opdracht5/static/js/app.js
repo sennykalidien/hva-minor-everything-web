e65cddce0b5ee3b1500ca7889463effd031ef811
Senny Kalidien <senny@directzichtbaar.nl>
2016-02-04 15:13:29 +0100

    - Added content + style to WebApp

--- webapp-from-scratch/opdrachten/week1/opdracht5/static/js/app.js
+++ webapp-from-scratch/opdrachten/week1/opdracht5/static/js/app.js
@@ -3,38 +3,37 @@ var APP = APP || {}; // Namespace als globale object. Zorgt ervoor dat je een ni
 (function () {
     'use strict'; // Scrict is een nieuwe feature in ECMAScript 5. Met EMCAscript5 kunnen we een programma of een functie in strict javascripttaal plaatsen. Het kijkt naar fouten en schakelt features uit die slecht zijn bedacht.
 
-    APP.myApp = { // Literal object =)
+    APP.fire = { // Literal object =)
         init: function () {
             APP.routes.init();
         }
     };
 
     APP.routes = {
-        init: function (event) {
-            window.addEventListener("hashchange", APP.sections.toggle); // if hashtag has change, toggle please.
-            window.addEventListener("load", APP.sections.toggle); // on load: toggle too please!
+        init: function () {                    
+            window.addEventListener("hashchange", function () { APP.sections.toggle(location.hash) }); // if hashtag has change, toggle please.
+            window.addEventListener("load", function () { APP.sections.toggle(location.hash) }); // on load: toggle too please!
         }
     };
 
     APP.sections = {
-        toggle: function () {
-            var currentHash = location.hash,
-                sections = document.querySelectorAll("section");
+        toggle: function (route) {
+            var sections = document.querySelectorAll("section");
             
             for (var i = 0; i < sections.length; i++) {
                 sections[i].classList.add('inactive'); // add inactive to ALL sections                                 
                 
                 // Default route
-                if (!currentHash) {
+                if (!route) {
                     sections[0].classList.remove('inactive');  // remove inactive                 
                 } else {
-                    document.querySelector(currentHash).classList.remove('inactive'); //remove inactive                   
+                    document.querySelector(route).classList.remove('inactive'); //remove inactive                   
                 }
             }
         }   
     };
 
-    APP.myApp.init();
+    APP.fire.init();
 
 })();
 
