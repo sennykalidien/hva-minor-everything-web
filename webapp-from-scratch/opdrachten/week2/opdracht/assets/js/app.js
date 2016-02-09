9914fb4fa9bdd7766644150a112481d0687c2d23
Senny Kalidien <senny@directzichtbaar.nl>
2016-02-09 15:08:47 +0100

    Kleine aanpassingen code

--- webapp-from-scratch/opdrachten/week2/opdracht/assets/js/app.js
+++ webapp-from-scratch/opdrachten/week2/opdracht/assets/js/app.js
@@ -32,7 +32,7 @@ var APP = APP || { };
 	***************************************************/
 	APP.router = { 	// Literal object: 'router'.  
 		init: function () { 				
-	  		routie({ //Routie kijkt naar wat achter de # komt achter de link en selecteert het.
+	  		routie({ //Routie kijkt naar wat achter de # komt van de link en selecteert dat.
 			    sections: function() {
 					APP.router.toggle(window.location.hash);
 			    },	    
@@ -43,12 +43,12 @@ var APP = APP || { };
 		},	
 		toggle: function (route) {            
             for (var i = 0; i < sections.length; i++) { // For loop to check all sections.
-                sections[i].classList.add('inactive'); // add inactive to ALL sections                                 
+                sections[i].classList.add('inactive'); // add inactive to ALL sections first.                                
                 
                 if (!route) {  // Default route
                     sections[0].classList.remove('inactive'); // remove inactive if no hashtag in the link                
                 } else {
-                    document.querySelector(route).classList.remove('inactive'); //remove inactive to the section that corresponds with the hashtag in the links.                    
+                    document.querySelector(route).classList.remove('inactive'); //remove inactive to the section that corresponds with the hashtag in the link.                    
                 }
             }
 		}
@@ -77,6 +77,7 @@ var APP = APP || { };
     			    apiKey = 'af7f18026c501a31c7a66eea851e85f4:9:74334837',
                     apiURL = 'http://api.nytimes.com/svc/topstories/v1/'+storySection+'.'+responseFormat+'?api-key='+apiKey+'';                   
                 
+                /* The Ajax Request (GET) with NanoAjax library */
                 var xhrRequest = nanoajax.ajax({
                         url: apiURL,
                         method: 'GET'
@@ -85,7 +86,8 @@ var APP = APP || { };
                         console.log(data);
                         Transparency.render(document.querySelector('[data-route="top-stories"]'), data, directives);                        
                 })	 
-
+                
+                /* Directives needed for Transparency to manipulate data-bind */
                 var directives = {
                     results: {
                         /* 
@@ -106,8 +108,8 @@ var APP = APP || { };
     						}	
 						}	
 					}
-	            }; // Sluit directives.	    		    	
-			} 				 
+	            };
+			}  // Sluit APP.page.topStories.get				 
 		} // Sluit APP.page.topStories
 	}; // Sluit APP.page.	
 	   
