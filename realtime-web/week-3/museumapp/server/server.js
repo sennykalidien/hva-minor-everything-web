ddb31205a47bd5f5197320f28808b88dfb4003b3
Senny Kalidien <senny@directzichtbaar.nl>
2016-05-16 19:40:56 +0200

    Fixed images issue dataset Amsterdam Musuem

--- realtime-web/week-3/museumapp/server/server.js
+++ realtime-web/week-3/museumapp/server/server.js
@@ -12,31 +12,28 @@ Meteor.startup(() => {
 });
 
 Meteor.publish('artSearch', function (query) {
+    searchQuery = query.split(' ').join('+');
+    console.log(searchQuery);
     var self = this;
     try {
-        var response = HTTP.get('http://amdata.adlibsoft.com/wwwopac.ashx?database=AMcollect&xmltype=grouped&search=title=' + query + '&limit=25&output=json');
+        var response = HTTP.get('http://amdata.adlibsoft.com/wwwopac.ashx?database=AMcollect&xmltype=grouped&search=title=%27'+searchQuery+'%27&limit=100&output=json');
 
         var data = response.data.adlibJSON.recordList.record;
         //console.log(data);
 
         [].forEach.call(data, function (item) {
 
-          if(item.hasOwnProperty('reproduction') != false) {
+          if((item.hasOwnProperty('reproduction') != false ) && (item.reproduction[0]['reproduction.identifier_URL'][0] != "" )) {
+            var image = item.reproduction[0]['reproduction.identifier_URL'][0].split('\\').pop(); // removes last element of array
             var object = {
                 title: item.title[0],
-                image: item.reproduction[0]['reproduction.reference'][0] ,
-                category: item.object_category[0],
-                creator: item.maker.creator
+                image: image,
+                category: item.object_category[0]
             };
-
             console.log(object);
-
-            self.added('search_art-objects', Random.id(), object);
-
+            self.added('search_art-objects', Random.id(), object); // Random.id = Meteor package
           }
-
         });
-
         self.ready();
 
     } catch (error) {
